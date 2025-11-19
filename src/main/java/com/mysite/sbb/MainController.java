d33fe05628e631781ae90475950cc07a3eac5330
lonelylonelylonely <lonelylonelylonely@example.com>
2025-11-19 15:21:46 +0900

    GetMapping으로 웹 브라우저에 직접출력

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,12 +2,16 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void index() {
+    @ResponseBody // 이 메서드가 반환하는 값을 HTTP 응답 본문에 출력하도록 설정
+    // 즉, "==^^==" 이 문자열이 웹 브라우저에 표시됨
+    public String index() {
         System.out.println("index() method called!");
+        return "==^^==";
     }
 
     @GetMapping("/hello")
