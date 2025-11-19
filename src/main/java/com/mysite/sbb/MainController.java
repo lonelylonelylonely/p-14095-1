f30de78b54975d8b9f9f325b167b03ee8dacf0fc
lonelylonelylonely <lonelylonelylonely@example.com>
2025-11-19 15:35:45 +0900

    5강

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    @ResponseBody // 이 메서드가 반환하는 값을 HTTP 응답 본문에 출력하도록 설정
+    @GetMapping("/") // 아래 메서드를 액션 메서드로 지정
+    // 액션 메서드: 웹 브라우저가 "/" 경로로 접속했을 때 실행되는 메서드
+    @ResponseBody // 이 메서드가 리턴하는 값을 응답으로 보냄 -> HTTP 응답 본문에 출력하도록 설정
     // 즉, "==^^==" 이 문자열이 웹 브라우저에 표시됨
     public String index() {
         System.out.println("index() method called!");
